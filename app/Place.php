e3cb21232a1e311e12ba4fd9d4f1c8be44939de4
Sheinehah <maui.sheinehah@gmail.com>
2018-02-22 10:58:54 -1000

    Model

--- app/Place.php
+++ app/Place.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Place extends Model
 {
-    //
+  /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+    protected $fillable = ['name', 'establishment_id'];
+
+   /**
+    * Get the establishment for the place.
+    */
+    public function users() 
+    {
+        return $this->hasOne('App\Establishment');
+    }
 }
